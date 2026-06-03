24c6d7052fb1052615d3e9f0906471b358123417
youngmb <young@cs.umanitoba.ca>
2026-06-03 16:17:10 -0500

    ongoing debugging of stream to sota

--- src/audioStreaming/AudioStreamPlayer.java
+++ src/audioStreaming/AudioStreamPlayer.java
@@ -19,7 +19,8 @@ public class AudioStreamPlayer implements Runnable {
     public final static int SYSTEM_BUFFER_SIZE = 112; // This is the system read boundary. Java 8 bug, match a multiple of this.
 
     private Integer ioBufferSize = null;
-    private Integer ioBufferSize_ms = null;
+    private int bytesPerFrame;
+    private int sampleRate;
 
     private final BlockingQueue<byte[]> playbackQueue;
     private Thread workerThread = null;
@@ -42,7 +43,7 @@ public class AudioStreamPlayer implements Runnable {
             // pactl info | grep "Default Sink"
             Mixer.Info[] mixerInfos = AudioSystem.getMixerInfo();
             for (Mixer.Info mixerInfo : mixerInfos) {
-                if (mixerInfo.getName().contains("plughw:2,0")) {
+                if (mixerInfo.getName().contains("hw:2,0")) {
                     Mixer mixer = AudioSystem.getMixer(mixerInfo);
                     DataLine.Info lineInfo = new DataLine.Info(SourceDataLine.class, format);
                     System.out.println("Audio Stream player line supported: " + mixerInfo.getName()+" "+format.toString());
@@ -76,6 +77,9 @@ public class AudioStreamPlayer implements Runnable {
                     Properties.getPropAsInt(PropertyKey.KEY_SPK_CHANNELS), true,false
                 )
         );
+        this.sampleRate = Properties.getPropAsInt(PropertyKey.KEY_SPK_SAMPLE_RATE);
+        this.bytesPerFrame = Properties.getPropAsInt(PropertyKey.KEY_SPK_SAMPLE_SIZE) / 8;
+
 //        ioBufferSize_ms = (int)(Math.ceil
 //                ( (double)ioBufferSize
 //                / Properties.getPropAsInt(PropertyKey.KEY_SPK_CHANNELS)
@@ -88,105 +92,118 @@ public class AudioStreamPlayer implements Runnable {
         workerThread.start();
     }
 
-
-    @Override
-    public void run() {     // Dedicated worker keeps playback off the network IO threads.
-        running = true;
-
-        byte[] silenceBuffer = new byte[ioBufferSize];
-
-        while (running) {
-            byte[] data = playbackQueue.poll();
-
-            if (data == null || data.length == 0) {
-                System.out.println("Starvation: write silence");
-                    writeData(silenceBuffer, ioBufferSize);
-            } else {
-                System.out.println("write data");
-                writeData (data, data.length);
-            }
-        }
-    }
-
-
+//
 //    @Override
 //    public void run() {     // Dedicated worker keeps playback off the network IO threads.
 //        running = true;
-//        workerThread = Thread.currentThread();
 //        byte[] silenceBuffer = new byte[ioBufferSize];
-//        byte[] data = null;
+//        long framesWritten = 0;
+//        int jitterBufferSize = 5;
+//        boolean fillJitterBuffer = true;
 //
 //        while (running) {
-//            try {
-////                data = playbackQueue.take();  // block only on empty
-//                data = playbackQueue.poll(1, TimeUnit.MILLISECONDS);
-//            } catch (InterruptedException e) { // continue gracefully if interrupted
-//                continue;
-//            }
-//            int qs = playbackQueue.size();
-//            if (qs > 1) System.err.println("Queue depth: " + qs);
-////            if (data == null || data.length == 0) {
-////                int silenceLen = Math.min(0, ioBufferSize-sourceLine.available());
-////                writeData(silenceBuffer, silenceLen);
-////            } else {
-////                writeData (data, data.length);
-////            }
-//            if (data == null || data.length == 0) {
-//                int avail = sourceLine.available();
-//                System.err.println("Queue empty: sdl.available()=" + avail + "/" + ioBufferSize);
-//                if (sourceLine.available() == ioBufferSize) {
-//                    // Hardware buffer fully drained - now we need silence
+//            long framesPlayed = sourceLine.getLongFramePosition();
+//            long framesBuffered = framesWritten - framesPlayed;
+//            long frameBufferThreshold = (ioBufferSize / this.bytes_per_frame);
+//
+//            System.out.println("framesWritten=" + framesWritten
+//                    + " framesPlayed=" + framesPlayed
+//                    + " framesBuffered=" + framesBuffered
+//                    + " threshold=" + frameBufferThreshold
+//                    + " playbackkqueue= "+playbackQueue.size());
+//
+//            if (fillJitterBuffer) {
+//                System.out.println("Waiting for buffer...");
+//                if (playbackQueue.size() < jitterBufferSize) {
+//                    LockSupport.parkNanos(1_000_000L);
+//                } else {
+//                    fillJitterBuffer = false;
+//                    System.out.println("Starting playback, queue depth: " + playbackQueue.size());
+//                }
+//                }
+//
+//            if (framesBuffered < frameBufferThreshold) { // less than a full packet left in the buffer
+//                byte[] data = playbackQueue.poll();
+//                if (data != null) {
+//                    writeData(data, data.length);
+//                    framesWritten += data.length / this.bytes_per_frame;
+//                    System.out.println("write data (playback queue "+playbackQueue.size()+")");
+//                } else {
+//                    // genuinely out of data relative to hardware position
 //                    writeData(silenceBuffer, ioBufferSize);
+//                    System.out.println("Starvation: write silence");
+//                    framesWritten += frameBufferThreshold;
+//                    fillJitterBuffer = true; // startover with jitter buffer
 //                }
 //            } else {
-//                writeData (data, data.length);
-//            }
-//            // JITTER CONTROL / CATCH-UP:
-//            // If the queue is building up, skip (drop) older packets to eliminate latency.
-//            // Do NOT try to write them all sequentially, or you will block and freeze!
-//            if (playbackQueue.size() > 10) {
-//                System.err.println("Audio Lag Detected! Skipping backed-up frames.");
-//                while (playbackQueue.size() > 1) {
-//                    playbackQueue.poll();
-//                }
+//                LockSupport.parkNanos(1_000_000L); // 1ms delay before trying again, avoid busy spin
+//                System.out.print(".");
 //            }
 //        }
 //    }
 
+
+
+    @Override
+    public void run() {     // Dedicated worker keeps playback off the network IO threads.
+        running = true;
+        byte[] silenceBuffer = new byte[ioBufferSize];
+
+        long startNs = System.nanoTime();
+        long framesScheduled = 0;
+        long framesInBuffer = (ioBufferSize / this.bytesPerFrame);
+
+        while (running) {
+
+            // how many frames should have played by now
+            long elapsedNs = System.nanoTime() - startNs;
+            long framesPlayed = (long)(elapsedNs / 1_000_000_000.0 * this.sampleRate);
+
+            System.out.println("framesScheduled=" + framesScheduled
+                            + " framesPlayed=" + framesPlayed
+                            + " framesInBuffer=" + framesInBuffer
+                            + " playbackkqueue= "+playbackQueue.size());
+
+            // only write when hardware should be ready for next packet
+            if (framesScheduled <= framesPlayed + framesInBuffer) {
+                byte[] data = playbackQueue.poll();
+                if (data != null) {
+                    System.out.println("write data (playback remaining queue "+playbackQueue.size()+")");
+                    writeData(data, data.length);
+                    framesScheduled += data.length / this.bytesPerFrame;
+                } else {
+                    System.out.println("Starvation. write silence");
+                    writeData(silenceBuffer, ioBufferSize);
+                    framesScheduled += framesInBuffer;
+                }
+            } else {
+                LockSupport.parkNanos(1_000_000L);
+                System.out.println("park");
+            }
+        }
+    }
+
+//    @Override
 //    public void run() {     // Dedicated worker keeps playback off the network IO threads.
-//        int frameSize = Properties.getPropAsInt(PropertyKey.KEY_SPK_CHANNELS) * Properties.getPropAsInt(PropertyKey.KEY_SPK_SAMPLE_SIZE)/8;
-//
-//        double s_in_buffer =
-//                (double) ioBufferSize / frameSize / Properties.getPropAsInt(PropertyKey.KEY_SPK_SAMPLE_RATE);
-//        long bufferNs = (long)(s_in_buffer * 1_000_000_000L);
-//
-//        long next = System.nanoTime();
-//
 //        running = true;
-//        workerThread = Thread.currentThread();
-//        byte[] data = null;
+//
+//        byte[] silenceBuffer = new byte[ioBufferSize];
 //
 //        while (running) {
-//            data = playbackQueue.poll();
+//            byte[] data = playbackQueue.poll();
 //
-//            if (data != null) // have data
-//                writeData(data);
-//            else
-//                writeData(new byte[ioBufferSize]); // write silence
-//
-//            if (playbackQueue.size() > Properties.getPropAsInt(PropertyKey.KEY_NET_UDP_WAITBUFFER_SIZE)) { // we're behind
-//                data = playbackQueue.poll();
-//                while (data !=  null) {
-//                    writeData(data);
-//                    data = playbackQueue.poll();
-//                }
-//                next = System.nanoTime();
+//            if (data == null || data.length == 0) {  // no incoming data
+////
+////            if (sourceLine.available()==ioBufferSize) {// empty buffer, its starving
+////                    System.out.println("Starvation: write silence");
+////                    writeData(silenceBuffer, ioBufferSize);
+////                } else {
+//                LockSupport.parkNanos(1_000_000L); // 1ms, hardware is still playing
+////                }
+//            } else {
+//                System.out.println("write data (queue size "+playbackQueue.size()+")");
+//                writeData (data, data.length);
 //            }
-//
-//            next += bufferNs;  // wait this much for every frame we receive
-//            long sleepNs = next - System.nanoTime();
-//            if (sleepNs > 0)
-//                LockSupport.parkNanos(sleepNs);
 //        }
 //    }
 
@@ -197,7 +214,7 @@ public class AudioStreamPlayer implements Runnable {
             while (written < datalen) {  // TODO : probably needs fixing to fix SOTA buffer underflow bug
                 int toWrite = Math.min(ioBufferSize, datalen - written);
                 int actuallyWritten = sourceLine.write(data, written, toWrite);
-//                System.out.println("actually written "+actuallyWritten);
+                System.out.println("actually written "+actuallyWritten + " ("+(ioBufferSize-actuallyWritten)+")");
                 written += actuallyWritten;
             }
         } catch (Exception e) {  // shouldn't happen
